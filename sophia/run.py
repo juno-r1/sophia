cdeca9e60255b04eeff1d57871fc280e83ddfdbb
juno-r1 <junoroberts9801@gmail.com>
2023-03-20 01:53:37 +0000

    finalise version

--- sophia/run.py
+++ sophia/run.py
@@ -2,5 +2,5 @@ import sophia
 
 if __name__ == '__main__':
 	
-	runtime = sophia.runtime('main.sph', 'task', 'instructions', 'namespace')
+	runtime = sophia.runtime('main.sph')
 	print(runtime.run())
